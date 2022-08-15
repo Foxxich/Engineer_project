24e30c2eb4cfb398cdf4fa4c7a4b9f9d296137d6
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-08-15 18:31:07 +0200

    Added extra methods

--- cnn.py
+++ cnn.py
@@ -1,7 +1,5 @@
-'''######################## Create CNN deep learning model ########################'''
 import pickle
 import time
-
 import tensorflow
 from keras.layers import Convolution2D
 from keras.layers import Dense
@@ -11,110 +9,108 @@ from keras.models import Sequential
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 
-# Specifying the folder where images are present
-TrainingImagePath = 'C:\\Users\\Vadym\\PycharmProjects\\Engineer_project\\Face Images\\Final Training Images\\'
 
-train_datagen = ImageDataGenerator(
-    shear_range=0.1,
-    zoom_range=0.1,
-    horizontal_flip=True)
+training_image_path = 'C:\\Users\\Vadym\\PycharmProjects\\Engineer_project\\Face Images\\Final Training Images\\'
+image_path = 'C:\\Users\\Vadym\\PycharmProjects\\Engineer_project\\Face Images\\Final Testing Images\\face1\\1.jpg'
 
-# Defining pre-processing transformations on raw images of testing data
-# No transformations are done on the testing images
-test_datagen = ImageDataGenerator()
 
-# Generating the Training Data
-training_set = train_datagen.flow_from_directory(
-    TrainingImagePath,
-    target_size=(64, 64),
-    batch_size=32,
-    class_mode='categorical',)
+# This function is used to prepare both training and final test sets.
+# First is prepared with defining pre-processing transformations on raw images of testing data.
+# The final test set is generated without transformations.
+def generate_sets():
+    test_datagen = ImageDataGenerator()
 
-# Generating the Testing Data
-test_set = test_datagen.flow_from_directory(
-    TrainingImagePath,
-    target_size=(64, 64),
-    batch_size=32,
-    class_mode='categorical',)
+    train_datagen = ImageDataGenerator(
+        shear_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True)
 
-'''############ Creating lookup table for all faces ############'''
-# class_indices have the numeric tag for each face
-TrainClasses = training_set.class_indices
+    training_set = train_datagen.flow_from_directory(
+        training_image_path,
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical', )
 
-# Storing the face and the numeric tag for future reference
-ResultMap = {}
-for faceValue, faceName in zip(TrainClasses.values(), TrainClasses.keys()):
-    ResultMap[faceValue] = faceName
+    test_set = test_datagen.flow_from_directory(
+        training_image_path,
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical', )
 
-with open("ResultsMap.pkl", 'wb') as fileWriteStream:
-    pickle.dump(ResultMap, fileWriteStream)
+    return training_set, test_set
 
-# The model will give answer as a numeric tag
-# This mapping will help to get the corresponding face name for it
-print("Mapping of Face and its ID", ResultMap)
 
-# The number of neurons for the output layer is equal to the number of faces
-OutputNeurons = len(ResultMap)
-print('\n The Number of output neurons: ', OutputNeurons)
+# This function consists of a few main functionalities:
+# 1. adding the first layer of CNN;
+# 2. max pooling and adding of additional layer of convolution for better accuracy;
+# 3. flattening;
+# 4. connecting of Neural Network;
+# This implementation is used with the format (64,64,3) for TensorFlow backend;
+# It is done to represent 3 matrix (with each size 64 to 64) for
+# representing Red, Green and Blue components of pixels;
+def prepare_classifier(output_neurons):
+    classifier = Sequential()
+    classifier.add(Convolution2D(32, kernel_size=(5, 5), strides=(1, 1), input_shape=(64, 64, 3), activation='relu'))
+    classifier.add(MaxPool2D(pool_size=(2, 2)))
+    classifier.add(Convolution2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
+    classifier.add(MaxPool2D(pool_size=(2, 2)))
+    classifier.add(Flatten())
+    classifier.add(Dense(64, activation='relu'))
+    classifier.add(Dense(output_neurons, activation='softmax'))
+    '''# Compiling the CNN'''
+    # classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    return classifier
 
-'''Initializing the Convolutional Neural Network'''
-classifier = Sequential()
 
-''' STEP--1 Convolution
-# Adding the first layer of CNN
-# we are using the format (64,64,3) because we are using TensorFlow backend
-# It means 3 matrix of size (64X64) pixels representing Red, Green and Blue components of pixels
-'''
-classifier.add(Convolution2D(32, kernel_size=(5, 5), strides=(1, 1), input_shape=(64, 64, 3), activation='relu'))
+def main():
+    training_set, test_set = generate_sets()
+    # class_indices have the numeric tag for each face
+    train_classes = training_set.class_indices
 
-'''# STEP--2 MAX Pooling'''
-classifier.add(MaxPool2D(pool_size=(2, 2)))
+    # Storing the face and the numeric tag for future reference
+    result_map = {}
+    for faceValue, faceName in zip(train_classes.values(), train_classes.keys()):
+        result_map[faceValue] = faceName
 
-'''############## ADDITIONAL LAYER of CONVOLUTION for better accuracy #################'''
-classifier.add(Convolution2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
+    with open("ResultsMap.pkl", 'wb') as fileWriteStream:
+        pickle.dump(result_map, fileWriteStream)
 
-classifier.add(MaxPool2D(pool_size=(2, 2)))
+    # The model will give answer as a numeric tag
+    # This mapping will help to get the corresponding face name for it
+    print("Mapping of Face and its ID", result_map)
 
-'''# STEP--3 FLattening'''
-classifier.add(Flatten())
+    # The number of neurons for the output layer is equal to the number of faces
+    output_neurons = len(result_map)
+    print('\n The Number of output neurons: ', output_neurons)
 
-'''# STEP--4 Fully Connected Neural Network'''
-classifier.add(Dense(64, activation='relu'))
+    classifier = prepare_classifier(output_neurons)
+    # Measuring the time taken by the model to train
+    start_time = time.time()
+    steps_per_epoch = len(test_set)
 
-classifier.add(Dense(OutputNeurons, activation='softmax'))
+    # Starting the model training
+    classifier.fit(
+        training_set,
+        steps_per_epoch=steps_per_epoch,
+        epochs=50,
+        validation_data=test_set,
+        validation_steps=10)
 
-'''# Compiling the CNN'''
-# classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    end_time = time.time()
+    print("###### Total Time Taken: ", round((end_time - start_time)), 'Seconds ######')
 
-'''# Compiling the CNN'''
-# classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    test_image = tensorflow.keras.utils.load_img(image_path, target_size=(64, 64))
+    test_image = tensorflow.keras.utils.img_to_array(test_image)
 
-# Measuring the time taken by the model to train
-StartTime = time.time()
+    test_image = np.expand_dims(test_image, axis=0)
 
-steps_per_epoch= len(test_set)
+    result = classifier.predict(test_image, verbose=0)
+    # print(training_set.class_indices)
 
-# Starting the model training
-classifier.fit(
-    training_set,
-    steps_per_epoch=steps_per_epoch,
-    epochs=50,
-    validation_data=test_set,
-    validation_steps=10)
+    print('####' * 10)
+    print('Prediction is: ', result_map[np.argmax(result)])
 
-EndTime = time.time()
-print("###### Total Time Taken: ", round((EndTime - StartTime) / 60), 'Minutes ######')
 
-ImagePath = 'C:\\Users\\Vadym\\PycharmProjects\\Engineer_project\\Face Images\\Final Testing Images\\face4\\3face4.jpg'
-test_image = tensorflow.keras.utils.load_img(ImagePath, target_size=(64, 64))
-test_image = tensorflow.keras.utils.img_to_array(test_image)
-
-test_image = np.expand_dims(test_image, axis=0)
-
-result = classifier.predict(test_image, verbose=0)
-# print(training_set.class_indices)
-
-print('####' * 10)
-print('Prediction is: ', ResultMap[np.argmax(result)])
+if __name__ == "__main__":
+    main()
