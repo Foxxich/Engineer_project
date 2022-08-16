440ceb08c27b1710b8e27b8830e9fd5f8ecc3f11
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-08-17 01:05:03 +0200

    Modified code to take image and go to registration

--- application.py
+++ application.py
@@ -1,85 +1,154 @@
+import time
 import tkinter as tk
 
-# program to capture single image from webcam in python
-
-# importing OpenCV library
-from tkinter import *
-
-from cv2 import *
-
-# camera
-# from cv2 import VideoCapture, imshow, imwrite, waitKey, destroyWindow
-#
-# cam_port = 0
-# cam = VideoCapture(cam_port)
-#
-# # reading the input using the camera
-# result, image = cam.read()
-#
-# # If image will detected without any error,
-# # show result
-# if result:
-#
-#     # showing result, it take frame name and image
-#     # output
-#     imshow("GeeksForGeeks", image)
-#
-#     # saving image in local storage
-#     imwrite("GeeksForGeeks.png", image)
-#
-#     # If keyboard interrupt occurs, destroy image
-#     # window
-#     waitKey(0)
-#     destroyWindow("GeeksForGeeks")
-#
-# # If captured image is corrupted, moving to else part
-# else:
-#     print("No image detected. Please! try again")
-def open_camera():
-    pass
+import PIL.Image
+import PIL.ImageTk
+import cv2
 
 
-def login():
-    open_camera()
+class App:
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+        self.ok = False
 
-def register():
-    master = Tk()
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = VideoCapture(self.video_source)
 
-    # this will create a label widget
-    l1 = Label(master, text="U need to provide your image to login!!")
+        # Create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
 
-    # grid method to arrange labels in respective
-    # rows and columns as specified
-    l1.grid(row=0, column=0, sticky=W, pady=2)
+        # Button that lets the user take a snapshot
+        self.btn_snapshot = tk.Button(window, text="Snapshot", command=self.snapshot)
+        self.btn_snapshot.pack(side=tk.LEFT)
 
-    # entry widgets, used to take entry from user
-    e1 = Entry(master)
+        # quit button
+        self.btn_quit = tk.Button(window, text='QUIT', command=quit)
+        self.btn_quit.pack(side=tk.LEFT)
 
-    # this will arrange entry widgets
-    e1.grid(row=0, column=1, pady=2)
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 10
+        self.update()
 
-    # checkbutton widget
-    c1 = Checkbutton(master, text="Preserve")
-    c1.grid(row=2, column=0, sticky=W, columnspan=2)
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
 
-    # button widget
-    b1 = Button(master, text="Zoom in")
-    b2 = Button(master, text="Zoom out")
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            self.vid.destroy()
+            self.window.destroy()
+            cv2.destroyAllWindows()
 
-    # arranging button widgets
-    b1.grid(row=2, column=2, sticky=E)
-    b2.grid(row=2, column=3, sticky=E)
+    def update(self):
+
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if self.ok:
+            self.vid.out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+        self.window.after(self.delay, self.update)
 
 
-window = tk.Tk()
+class VideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
 
-window.columnconfigure([0, 1, 2], minsize=111, weight=1)
-window.rowconfigure([0, 1, 2], minsize=111, weight=1)
+        # 1. Video Type
+        VIDEO_TYPE = {
+            'avi': cv2.VideoWriter_fourcc(*'XVID'),
+            # 'mp4': cv2.VideoWriter_fourcc(*'H264'),
+            'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+        }
 
-btn_decrease = tk.Button(master=window, text="login", command=login)
-btn_decrease.grid(row=1, column=0, sticky=E, pady = 3)
+        self.fourcc = VIDEO_TYPE['avi']
 
-btn_increase = tk.Button(master=window, text="register", command=register)
-btn_increase.grid(row=1, column=2, sticky=W, pady = 3)
+        # 2. Video Dimension
+        STD_DIMENSIONS = {
+            '480p': (640, 480),
+            '720p': (1280, 720),
+            '1080p': (1920, 1080),
+            '4k': (3840, 2160),
+        }
+        res = STD_DIMENSIONS['480p']
+        print('output', self.fourcc, res)
+        self.out = cv2.VideoWriter('output .' + 'avi', self.fourcc, 10, res)
 
-window.mainloop()
+        # set video sourec width and height
+        self.vid.set(3, res[0])
+        self.vid.set(4, res[1])
+
+        # Get video source width and height
+        self.width, self.height = res
+
+    # To get frames
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+            self.out.release()
+            cv2.destroyAllWindows()
+
+    def destroy(self):
+        self.vid.release()
+
+
+class RegisterWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+
+    def close_windows(self):
+        self.master.destroy()
+
+
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master, width=300, height=300)
+        self.frame.size()
+        self.button1 = tk.Button(self.frame, text = 'Login', width = 125, command = self.login_window)
+        self.button1.pack()
+        self.button2 = tk.Button(self.frame, text = 'Register', width = 125, command = self.register_window)
+        self.button2.pack()
+        self.frame.pack()
+
+    def login_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = App(self.newWindow, 'Video Recorder')
+
+
+    def register_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = RegisterWindow(self.newWindow)
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("300x300")
+    app = MainWindow(root)
+    root.mainloop()
+
+main()
