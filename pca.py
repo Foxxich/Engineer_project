9389b649cfa6d6a6dc61607b3b95411487c6c562
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-08-20 17:48:04 +0200

    Chose better implementation of the PCA

--- pca.py
+++ pca.py
@@ -6,97 +6,103 @@ import numpy as np
 from PIL import Image
 from sklearn.decomposition import PCA
 
-# Read face image from zip file on the fly
-faces = {}
-path = os.getcwd() + "\\att_faces\\"
-image_folders = os.listdir(path)
-for i in range(1, len(image_folders) + 1):
-    filepath = path + str(i) + "\\*.pgm"
-    files_list = glob.glob(filepath)
-    for j in range(1, len(files_list) + 1):
-        image_path = path + str(i) + "\\" + str(j) + ".pgm"
-        order = str(i) + "/" + str(j) + ".pgm"
-        img = Image.open(image_path)
-        img.load()
-        data = np.asarray(img, dtype="int32")
-        # If we extracted files from zip, we can use cv2.imread(filename) instead
 
-        faces[order] = data
-print(len(faces))
-print(faces)
+def prepare_data_set():
+    faces = {}
+    path = os.getcwd() + "\\att_faces\\"
+    image_folders = os.listdir(path)
+    for i in range(1, len(image_folders) + 1):
+        filepath = path + str(i) + "\\*.pgm"
+        files_list = glob.glob(filepath)
+        for j in range(1, len(files_list) + 1):
+            image_path = path + str(i) + "\\" + str(j) + ".pgm"
+            order = str(i) + "/" + str(j) + ".pgm"
+            img = Image.open(image_path)
+            img.load()
+            data = np.asarray(img, dtype="int32")
+            # If we extracted files from zip, we can use cv2.imread(filename) instead
 
-# Show sample faces using matplotlib
-fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 10))
-faceimages = list(faces.values())[-16:]  # take last 16 images
-for i in range(16):
-    axes[i % 4][i // 4].imshow(faceimages[i], cmap="gray")
-print("Showing sample faces")
-plt.show()
+            faces[order] = data
+    return faces
 
-# Print some details
-faceshape = list(faces.values())[0].shape
-print("Face image shape:", faceshape)
 
-classes = set(filename.split("/")[0] for filename in faces.keys())
-print("Number of classes:", len(classes))
-print("Number of images:", len(faces))
+def main():
+    faces = prepare_data_set()
+    fig, axes = plt.subplots(4, 4, figsize=(8, 10))
+    # faceimages = list(faces.values())[-16:]  # last 16 images
+    # for i in range(16):
+    #     axes[i % 4][i // 4].imshow(faceimages[i], cmap="gray")
+    # print("Showing sample faces")
+    # plt.show()
 
-# Take classes 1-39 for eigenfaces, keep entire class 40 and
-# image 10 of class 39 as out-of-sample test
-facematrix = []
-facelabel = []
-for key, val in faces.items():
-    if key.startswith("39/"):
-        continue  # this is our test set
-    if key == "39/9.pgm":
-        continue  # this is our test set
-    facematrix.append(val.flatten())
-    facelabel.append(key.split("/")[0])
+    # Print some details
+    face_shape = list(faces.values())[0].shape
+    print("Face image shape:", face_shape)
 
-# Create a NxM matrix with N images and M pixels per image
-facematrix = np.array(facematrix)
+    classes = set(filename.split("/")[0] for filename in faces.keys())
+    print("Number of classes:", len(classes))
+    print("Number of images:", len(faces))
 
-# Apply PCA and take first K principal components as eigenfaces
-pca = PCA().fit(facematrix)
+    # Take classes 1-39 for eigenfaces, keep entire class 40 and
+    # image 10 of class 39 as out-of-sample test
+    face_matrix = []
+    face_label = []
+    for key, val in faces.items():
+        if key.startswith("40/"):
+            continue  # this is our test set
+        if key == "39/10.pgm":
+            continue  # this is our test set
+        face_matrix.append(val.flatten())
+        face_label.append(key.split("/")[0])
 
-n_components = 50
-eigenfaces = pca.components_[:n_components]
+    # Create a NxM matrix with N images and M pixels per image
+    face_matrix = np.array(face_matrix)
 
-# Show the first 16 eigenfaces
-fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 10))
-for i in range(16):
-    axes[i % 4][i // 4].imshow(eigenfaces[i].reshape(faceshape), cmap="gray")
-print("Showing the eigenfaces")
-plt.show()
+    # Apply PCA and take first K principal components as eigenfaces
+    pca = PCA().fit(face_matrix)
 
-# Generate weights as a KxN matrix where K is the number of eigenfaces and N the number of samples
-weights = eigenfaces @ (facematrix - pca.mean_).T
-print("Shape of the weight matrix:", weights.shape)
+    n_components = 50
+    eigenfaces = pca.components_[:n_components]
 
-# Test on out-of-sample image of existing class
-query = faces["39/9.pgm"].reshape(1, -1)
-query_weight = eigenfaces @ (query - pca.mean_).T
-euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
-best_match = np.argmin(euclidean_distance)
-print("Best match %s with Euclidean distance %f" % (facelabel[best_match], euclidean_distance[best_match]))
-# Visualize
-fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(8, 6))
-axes[0].imshow(query.reshape(faceshape), cmap="gray")
-axes[0].set_title("Query")
-axes[1].imshow(facematrix[best_match].reshape(faceshape), cmap="gray")
-axes[1].set_title("Best match")
-plt.show()
+    # Show the first 16 eigenfaces
+    fig, axes = plt.subplots(4, 4, figsize=(8, 10))
+    for i in range(16):
+        axes[i % 4][i // 4].imshow(eigenfaces[i].reshape(face_shape), cmap="gray")
+    print("Showing the eigenfaces")
+    plt.show()
 
-# Test on out-of-sample image of new class
-query = faces["39/1.pgm"].reshape(1, -1)
-query_weight = eigenfaces @ (query - pca.mean_).T
-euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
-best_match = np.argmin(euclidean_distance)
-print("Best match %s with Euclidean distance %f" % (facelabel[best_match], euclidean_distance[best_match]))
-# Visualize
-fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(8, 6))
-axes[0].imshow(query.reshape(faceshape), cmap="gray")
-axes[0].set_title("Query")
-axes[1].imshow(facematrix[best_match].reshape(faceshape), cmap="gray")
-axes[1].set_title("Best match")
-plt.show()
+    # Generate weights as a KxN matrix where K is the number of eigenfaces and N the number of samples
+    weights = eigenfaces @ (face_matrix - pca.mean_).T
+    print("Shape of the weight matrix:", weights.shape)
+
+    # Test on out-of-sample image of existing class
+    query = faces["39/10.pgm"].reshape(1, -1)
+    query_weight = eigenfaces @ (query - pca.mean_).T
+    euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
+    best_match = np.argmin(euclidean_distance)
+    print("Best match %s with Euclidean distance %f" % (face_label[best_match], euclidean_distance[best_match]))
+    # Visualize
+    fig, axes = plt.subplots(1, 2, figsize=(8, 6))
+    axes[0].imshow(query.reshape(face_shape), cmap="gray")
+    axes[0].set_title("Query")
+    axes[1].imshow(face_matrix[best_match].reshape(face_shape), cmap="gray")
+    axes[1].set_title("Best match")
+    plt.show()
+
+    # Test on out-of-sample image of new class
+    query = faces["40/1.pgm"].reshape(1, -1)
+    query_weight = eigenfaces @ (query - pca.mean_).T
+    euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
+    best_match = np.argmin(euclidean_distance)
+    print("Best match %s with Euclidean distance %f" % (face_label[best_match], euclidean_distance[best_match]))
+    # Visualize
+    fig, axes = plt.subplots(1, 2, figsize=(8, 6))
+    axes[0].imshow(query.reshape(face_shape), cmap="gray")
+    axes[0].set_title("Query")
+    axes[1].imshow(face_matrix[best_match].reshape(face_shape), cmap="gray")
+    axes[1].set_title("Best match")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
