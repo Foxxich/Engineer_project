dde13f7632d6a6463592e5ebdca1bd5b51945902
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-09-27 17:54:44 +0200

    Final version of PCA, including new comments, solution of errors

--- algorithms/pca.py
+++ algorithms/pca.py
@@ -8,9 +8,8 @@ from sklearn.decomposition import PCA
 
 
 # noinspection PyTypeChecker
-
-
-# This function is used to load images from path of test set
+# This function is used to load images from path of test set,
+# where all images are located in special folders
 def load_data_set(path):
     faces = {}
     image_folders = os.listdir(path)
@@ -27,9 +26,11 @@ def load_data_set(path):
     return faces
 
 
+# noinspection PyTypeChecker
+# This function is used to load images from path of user images
+# or any other images, which are located in `common folder`
 def load_set(path):
     faces = {}
-    final_faces = {}
     image_folders = os.listdir(path)
     for i in range(1, len(image_folders)):
         if '.jpg' not in image_folders[i]:
@@ -43,7 +44,13 @@ def load_set(path):
     return faces
 
 
-def comparison(test_filename, path, data_type):
+# This function is used to run code both for tests/app
+# (has different ways of face_shapes loading),
+# implementing the logic of PCA, like:
+# 1. Create NxM matrix with N images and M pixels per image
+# 2. Apply PCA to use first K principal components as eigenfaces
+# 3. Prepare KxN matrix (K is the number of eigenfaces, N is the number of samples)
+def comparison(test_filename, path, data_type, n_components=100):
     if data_type == 'test':
         faces = load_data_set(path)
     else:
@@ -61,28 +68,22 @@ def comparison(test_filename, path, data_type):
         face_matrix.append(val.flatten())
         face_label.append(key.split("/")[0])
 
-    # Create NxM matrix with N images and M pixels per image
     face_matrix = np.array(face_matrix)
-    # Apply PCA to use first K principal components as eigenfaces
     pca = PCA().fit(face_matrix)
-    n_components = 100
     eigenfaces = pca.components_[:n_components]
-    # Generate weights as a KxN matrix where K is the number of eigenfaces and N the number of samples
     weights = eigenfaces @ (face_matrix - pca.mean_).T
     print("Shape of the weight matrix:", weights.shape)
-    # Test on out-of-sample image of existing class
-
     load_test_file = Image.open(test_filename)
     load_test_file.load()
+    # noinspection PyTypeChecker
     data = np.asarray(load_test_file, dtype="int32")
-    test = {'test': data}
+    image = {'image': data}
 
-    query = test['test'].reshape(1, -1)
+    query = image['image'].reshape(1, -1)
     query_weight = eigenfaces @ (query - pca.mean_).T
     euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
     best_match = np.argmin(euclidean_distance)
     print("Best match %s with Euclidean distance %f" % (face_label[best_match], euclidean_distance[best_match]))
-    # Visualize
     fig, axes = plt.subplots(1, 2, figsize=(8, 6))
     axes[0].imshow(query.reshape(face_shape), cmap="gray")
     axes[0].set_title("Query")
