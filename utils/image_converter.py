ffb9b31c2db0766b488a9fd9c0fd76b3f6aa2178
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-09-28 13:50:28 +0200

    Prepared all functionalities for image conversion

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -10,6 +10,8 @@ from PIL import Image, ImageFilter
 import definitons
 
 
+# This function is used to convert .PGM files to .JPG
+# with changing images formats from BGR to RGB
 def convert_formats():
     path = definitons.root_dir + "\\images\\att_faces_dataset\\"
     image_folders = os.listdir(path)
@@ -35,20 +37,46 @@ def convert_rgb_to_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-def blur(image_path, blur_percent):
-    return Image.open(image_path).filter(ImageFilter.GaussianBlur(blur_percent))
+# This function is used to add blur for image with given path, image_type, percent (default is 5)
+# and later save it to `blured\\image_type` folder
+def blur(folder_path, image_name, blur_percent=5):
+    Image.open(definitons.root_dir + "\\images\\" + folder_path + "\\" + image_name + ".jpg") \
+        .filter(ImageFilter.GaussianBlur(blur_percent)) \
+        .save(definitons.root_dir + "\\images\\blured\\" + folder_path + "\\" + image_name + ".jpg")
+    return definitons.root_dir + "\\images\\blured\\" + folder_path + "\\" + image_name + ".jpg"
 
 
-def gaussian_noise(image_path):
-    img = cv2.imread(image_path)
+# This function is used to add noise for image with given path, image_type
+# and later save it to `noised\\image_type` folder
+def gaussian_noise(folder_path, image_name):
+    img = cv2.imread(definitons.root_dir + "\\images\\" + folder_path + "\\" + image_name + ".jpg")
     gauss = np.random.normal(0, 1, img.size)
-    gauss = gauss.reshape(img.shape[0], img.shape[1], img.shape[2]).astype('uint8')
-    return cv2.add(img, gauss)
+    gauss = gauss.reshape((img.shape[0], img.shape[1], img.shape[2])).astype('uint8')
+    noise = img + img * gauss
+    cv2.imwrite(definitons.root_dir + "\\images\\noised\\" + folder_path + "\\" + image_name + ".jpg", noise)
+    return definitons.root_dir + "\\images\\noised\\" + folder_path + "\\" + image_name + ".jpg"
 
 
-def main():
-    blur(definitons.root_dir + "\\images\\user_images\\new_image.jpg", 5)
+# This function is used to add noise for image with given path, image_type
+# and later save it to `gray_noised\\image_type` folder;
+# The main difference between this and `gaussian_noise` conversions is
+# that image is converted to GRAYSCALE
+def add_gray_noise(folder_path, image_name):
+    image = cv2.imread(definitons.root_dir + "\\images\\" + folder_path + "\\" + image_name + ".jpg",
+                       cv2.IMREAD_GRAYSCALE)
+    row, col = image.shape
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+        x_coord = random.randint(0, col - 1)
+        image[y_coord][x_coord] = 255
 
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+        x_coord = random.randint(0, col - 1)
+        image[y_coord][x_coord] = 0
 
-if __name__ == "__main__":
-    main()
+    # Storing the image
+    cv2.imwrite(definitons.root_dir + "\\images\\gray_noised\\" + folder_path + "\\" + image_name + ".jpg", image)
+    return definitons.root_dir + "\\images\\gray_noised\\" + folder_path + "\\" + image_name + ".jpg"
