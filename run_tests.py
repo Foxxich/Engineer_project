f6b3d85eb2a8e5d3d6f9a0a8e0986c9d9ad344d5
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-09-30 17:03:09 +0200

    Prepared generating of results for SIFT, VGG, CNN

--- run_tests.py
+++ run_tests.py
@@ -6,63 +6,133 @@ import time
 import definitons
 from algorithms import sift, vgg_face, cnn, pca
 from utils.image_converter import blur, gaussian_noise
+from utils.files_utils import write
 
 # First element is set by default for running in every algorithm
 cnn_optimizers = ['adam', 'rmsprop', 'Ftrl', 'Nadam', 'Adamax']
 cnn_loss = ['categorical_crossentropy', 'binary_crossentropy']
 cnn_metrics = ['accuracy', 'binary_accuracy', 'categorical_accuracy', 'top_k_categorical_accuracy']
-cnn_epochs_number = [50, 45, 40, 35, 30, 25, 20, 15, 10]
-cnn_steps_for_validation = [10, 9, 8, 7, 6, 5]
-vgg_thresh = [0.5, 0.6, 0.7, 0.8, 0.4, 0.3]
+cnn_epochs_number = [50, 45, 20, 15, 10]
+cnn_steps_for_validation = [10, 9, 8, 7, 6]
+vgg_thresh = [0.5, 0.6, 0.7, 0.4, 0.3]
 vgg_model = ['resnet50', 'vgg16', 'senet50']
-pca_components = [100, 90, 80, 70, 60, 50]
+pca_components = [100, 90, 80, 70, 60]
 sift_cascades = ['haarcascade_frontalface_default',
                  'haarcascade_frontalface_alt',
                  'haarcascade_frontalface_alt_tree',
                  'haarcascade_frontalface_alt2']
-sift_percent_delta = [2.0, 2.5, 3.0, 5.0, 1.5, 1.0]
+sift_percent_delta = [2.0, 2.5, 4.0, 1.5, 1.0]
 blur_percents = [1, 2, 3, 4, 5]
+
+datasets = [
+    ['images\\datasets\\tt_dataset\\Final Training Images', 'training_dataset'],
+    ['images\\datasets\\tt_dataset\\Final Testing Images', 'testing_dataset'],
+]
+
+test_data = [
+    ['face1\\1face1.jpg', 'usual', 'tt_dataset', datasets[0][0], 'face1'],
+    ['face1\\2face1.jpg', 'usual', 'tt_dataset', datasets[0][0], 'face1'],
+]
 
 
 def run_sift():
-    test_image = definitons.root_dir + '\\images\\tests\\random_images\\1.jpg'
-    original_image = definitons.root_dir + '\\images\\tests\\random_images\\2.jpg'
-    start_time = time.time()
-    sift.comparison(test_image, original_image, sift_cascades[0])
-    end_time = time.time()
-    print("Total time: ", round((end_time - start_time)), ' Seconds')
+    data = []
+    for image1 in test_data:
+        image2 = None
+        for score in test_data:
+            if score[2] == image1[2] and score[0] != image1[0]:
+                image2 = score
+        test_image = definitons.root_dir + '\\' + image1[3] + '\\' + image1[0]
+        original_image = definitons.root_dir + '\\' + image2[3] + '\\' + image2[0]
+        start_time = time.time()
+        print(test_image)
+        res = sift.comparison(test_image, original_image, sift_cascades[0])
+        end_time = time.time()
+        is_same_person = False
+        if image1[4] == image2[4]:
+            is_same_person = True
+        data.append([
+            str(image1[0]),
+            str(image2[0]),
+            str(res),
+            str(is_same_person),
+            str(round((end_time - start_time))),
+            image1[2],
+            image1[1],
+        ])
+        print("Total time: ", round((end_time - start_time)), ' Seconds')
+    write(data, 'sift_cascades')
 
 
 def run_vgg():
-    test_image = definitons.root_dir + '\\images\\tests\\random_images\\1.jpg'
-    original_image = definitons.root_dir + '\\images\\tests\\random_images\\2.jpg'
-    start_time = time.time()
-    result = vgg_face.comparison(test_image, original_image, vgg_model[0],
-                                 vgg_thresh[0])
-    end_time = time.time()
-    print("Total time: ", round((end_time - start_time)), ' Seconds')
+    data = []
+    for image1 in test_data:
+        image2 = None
+        for score in test_data:
+            if score[2] == image1[2] and score[0] != image1[0]:
+                image2 = score
+        test_image = definitons.root_dir + '\\' + image1[3] + '\\' + image1[0]
+        original_image = definitons.root_dir + '\\' + image2[3] + '\\' + image2[0]
+        start_time = time.time()
+        print(test_image)
+        res = vgg_face.comparison(test_image, original_image, vgg_model[0],
+                                  vgg_thresh[0])
+        end_time = time.time()
+        is_same_person = False
+        if image1[4] == image2[4]:
+            is_same_person = True
+        data.append([
+            str(image1[0]),
+            str(image2[0]),
+            str(res),
+            str(is_same_person),
+            str(round((end_time - start_time))),
+            image1[2],
+            image1[1],
+        ])
+        print("Total time: ", round((end_time - start_time)), ' Seconds')
+    write(data, 'vgg_model')
 
 
 def run_cnn():
-    face = 2
-    random_image_number = random.randrange(1, 4, 1)
-    folder = definitons.root_dir + '\\images\\datasets\\tt_dataset\\Final Training Images\\'
-    image_path = definitons.root_dir + '\\images\\datasets\\tt_dataset\\Final Testing Images\\face' + str(
-        face) + '\\' + str(
-        random_image_number) + 'face' + str(face) + '.jpg'
-    start_time = time.time()
-    if cnn.comparison(folder,
-                      image_path,
-                      cnn_epochs_number[0],
-                      cnn_steps_for_validation[0],
-                      cnn_optimizers[0],
-                      cnn_loss[0],
-                      cnn_metrics[0]) == 'face' + str(face):
-        print('Same person on both images')
-    else:
-        print('Different persons on both images')
-    end_time = time.time()
-    print("Total time: ", round((end_time - start_time)), ' Seconds')
+    data = []
+    for image1 in test_data:
+        image2 = None
+        for score in test_data:
+            if score[2] == image1[2] and score[0] != image1[0]:
+                image2 = score
+        face = image2[0].replace("\\", " ").split()[0]
+        print(face)
+        folder = definitons.root_dir + '\\' + image1[3] + '\\'
+        print(folder)
+        image_path = definitons.root_dir + '\\' + image2[3] + '\\' + image2[0]
+        print(image_path)
+        start_time = time.time()
+        res = False
+        if cnn.comparison(folder,
+                          image_path,
+                          cnn_epochs_number[0],
+                          cnn_steps_for_validation[0],
+                          cnn_optimizers[0],
+                          cnn_loss[0],
+                          cnn_metrics[0]) == str(face):
+            res = True
+
+        end_time = time.time()
+        is_same_person = False
+        if image1[4] == image2[4]:
+            is_same_person = True
+        data.append([
+            str(image1[0]),
+            str(image2[0]),
+            str(res),
+            str(is_same_person),
+            str(round((end_time - start_time))),
+            image1[2],
+            image1[1],
+        ])
+        print("Total time: ", round((end_time - start_time)), ' Seconds')
+    write(data, 'cnn')
 
 
 def run_pca():
@@ -126,13 +196,10 @@ def run_image_selection(folder, images_number, modification_type):
 
 
 def main():
-    pass
-    # generate_blured_images()
-    # generate_gaussian()
     # run_sift()
     # run_vgg()
     # run_cnn()
-    # run_pca()
+    run_pca()
 
 
 if __name__ == "__main__":
