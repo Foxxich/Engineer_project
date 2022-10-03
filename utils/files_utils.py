fb6f02bb05ddd8922f8659d85e993b1170f44055
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-10-03 16:02:20 +0200

    Modified functionality to have different headers

--- utils/files_utils.py
+++ utils/files_utils.py
@@ -2,13 +2,19 @@ import csv
 
 import definitons
 
-headers = ['image', 'compare_with', 'result', 'is_same_person', 'time', 'dataset', 'type']
+headers = [
+    ['image', 'compare_with', 'result', 'is_same_person', 'time', 'dataset', 'type'],
+    ['folder', 'image', 'was_found', 'time', 'dataset', 'type']
+]
 
 
-def write(data, file_name):
+def write(data, file_name, header_type):
+    header = headers[0]
+    if header_type == 'complex':
+        header = headers[1]
     with open(definitons.root_dir + '\\' + file_name + '.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(headers)
+        writer.writerow(header)
         writer.writerows(data)
 
 
