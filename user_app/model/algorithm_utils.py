5102df63d16659f52a5ca00d0181f3026a5dbe0d
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-12-11 19:55:36 +0100

    Improved direction of located files

--- user_app/model/algorithm_utils.py
+++ user_app/model/algorithm_utils.py
@@ -12,8 +12,8 @@ from user_app.gui.logged_window import LoggedWindow
 
 def run_algorithm(self, main_window, algorithm_type):
     result = False
-    img1 = definitons.root_dir + '\\images\\user_images\\previous_images\\previous_image.jpg'
-    img2 = definitons.root_dir + '\\images\\user_images\\new_image.jpg'
+    img1 = definitons.user_images_dir + 'previous_images\\previous_image.jpg'
+    img2 = definitons.user_images_dir + 'new_image.jpg'
     start_time = time.time()
 
     try:
@@ -22,25 +22,25 @@ def run_algorithm(self, main_window, algorithm_type):
         elif algorithm_type == 'vgg':
             result = vgg_face.comparison(img1, img2)
         elif algorithm_type == 'pca':
-            path = definitons.root_dir + '\\images\\user_images\\'
+            path = definitons.user_images_dir
             start_time = time.time()
             face_name = pca.comparison(img2, path, 'app')
             if face_name == 'previous_images.jpg':
                 result = True
         elif algorithm_type == 'cnn':
             n = 20
-            folder = definitons.root_dir + '\\images\\user_images\\'
+            folder = definitons.user_images_dir
             for i in range(n):
-                file = definitons.root_dir + '\\images\\user_images\\previous_images\\previous_image' + str(
+                file = definitons.user_images_dir + 'previous_images\\previous_image' + str(
                     randrange(100)) + '.jpg'
                 shutil.copy(img1, file)
             cnn = CNN(folder, img2, 'user')
             face_name = cnn.comparison()
             filenames = \
-            next(os.walk(definitons.root_dir + '\\images\\user_images\\previous_images\\'), (None, None, []))[2]
+            next(os.walk(definitons.user_images_dir + 'previous_images\\'), (None, None, []))[2]
             for file in filenames:
                 if 'previous_image.jpg' not in file:
-                    os.remove(definitons.root_dir + '\\images\\user_images\\previous_images\\' + file)
+                    os.remove(definitons.user_images_dir + 'previous_images\\' + file)
             if face_name == 'previous_images':
                 result = True
         elif algorithm_type == 'initial':
@@ -54,10 +54,10 @@ def run_algorithm(self, main_window, algorithm_type):
         self.newWindow = tk.Toplevel(self.master)
         self.app = LoggedWindow(self.newWindow, main_window, 'success')
     else:
-        os.rename(definitons.root_dir + '\\images\\user_images\\previous_images\\previous_image.jpg',
-                  definitons.root_dir + '\\images\\user_images\\previous_images\\new_image.jpg')
-        os.replace(definitons.root_dir + '\\images\\user_images\\previous_images\\new_image.jpg',
-                   definitons.root_dir + '\\images\\user_images\\new_image.jpg')
+        os.rename(definitons.user_images_dir + 'previous_images\\previous_image.jpg',
+                  definitons.user_images_dir + 'previous_images\\new_image.jpg')
+        os.replace(definitons.user_images_dir + 'previous_images\\new_image.jpg',
+                   definitons.user_images_dir + 'new_image.jpg')
 
         self.newWindow = tk.Toplevel(self.master)
         self.app = LoggedWindow(self.newWindow, main_window, 'fail')
